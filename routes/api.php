fda6232b2c61a2e87da5fd37d17bb2ea4e4815c4
Julio <julio.campos@iebtdigital.com.br>
2022-01-31 21:06:05 -0300

    dando grupo para rotas, assim começam a serem autenticas para cada user

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,13 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/verify/{urlId}', [VerifyUrlController::class, 'show']);
-Route::post('/verify', [VerifyUrlController::class, 'store']);
-Route::put('/verify/{urlId}', [VerifyUrlController::class, 'update']);
 
+
+/* Grupo de Rotas que obriga o usuario estar logado */
+Route::group(['middleware' => ['auth:sanctum']], function(){
+
+    Route::get('/verify/{urlId}', [VerifyUrlController::class, 'show']);
+    Route::post('/verify', [VerifyUrlController::class, 'store']);
+    Route::put('/verify/{urlId}', [VerifyUrlController::class, 'update']);
+    Route::delete('/verify/{urlId}', [VerifyUrlController::class, 'destroy']);
+});
